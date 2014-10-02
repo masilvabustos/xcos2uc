4c3417bf271eccf5075eea22eea76b6cd9b82f51
Matías Silva Bustos <masilvabustos@ieee.org>
2014-10-02 11:26:50 -0300

    completed:   ../../platform/stm32vldiscovery/Project/GNUmakefile added some #includes:   ../../platform/stm32vldiscovery/Utilities/STM32vldiscovery.h

--- platform/stm32vldiscovery/Utilities/STM32vldiscovery.h
+++ platform/stm32vldiscovery/Utilities/STM32vldiscovery.h
@@ -28,6 +28,10 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f10x.h"
+#include "stm32f10x_gpio.h"
+#include "stm32f10x_rcc.h"
+#include "stm32f10x_exti.h"
+#include "misc.h"
 
 /** @addtogroup Utilities
   * @{
