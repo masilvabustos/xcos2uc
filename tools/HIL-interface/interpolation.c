85e5a6f456e23fcee33e0c2576e7ffd7935d2282
Matías Silva Bustos <masilvabustos@ieee.org>
2014-10-05 22:56:13 -0300

    modified:   interpolation.c

--- tools/HIL-interface/interpolation.c
+++ tools/HIL-interface/interpolation.c
@@ -4,54 +4,67 @@
 
 typedef float number_t;
 
-number_t lagrange_polynomials[][];
-
 struct nevilles_algorithm_workspace {
 	number_t *C;
 	number_t *D;
 	number_t *e;
 };
 
-struct nevilles_algorithm_parameters {
-	number_t t[];
-	number_t x[];
+struct point {
+	number_t t;
+	number_t x;
 };
 
+#define nevilles_algorithm_workspace_size(N) (sizeof(number_t) * (2 * N + 1))
 
-number_t nevilles_algorithm(unsigned N, number_t t, struct nevilles_algorithm_parameters p, struct nevilles_algorithm_workspace ws)
+void setup_nevilles_algorithm_workspace(unsigned N, struct nevilles_algorithm_workspace* ws, void * ptr)
 {
-	number_t x;
+	ws->C = ptr;
+	ws->D = (void*) ws->C + sizeof(number_t)*N;
+	ws->e = (void*) ws->D;
+}
+
+
+number_t nevilles_algorithm(unsigned N, number_t _t, number_t t[], number_t x[], struct nevilles_algorithm_workspace ws)
+{
+	number_t _x, dx;
 
 	float dif, dift;
 	int i, m, ns;
 
-	dif=fabs(t-p.t[0]);
+	dif=fabs(_t-t[0]);
 	for (i=0;i<N;i++) {
-		if ( (dift=fabs(t-t[i])) < dif) {
+		if ( (dift=fabs(_t-t[i])) < dif) {
 			ns=i;
 			dif=dift;
 		}
 	}
 
-	memmove(&ws.C[0], &p.x[0], N);
-	memmove(&ws.D[0], &p.x[0], N);
+	memmove(&ws.C[0], &x[0], N);
+	memmove(&ws.D[0], &x[0], N);
 
-	x=p.x[ns--];
+	_x=x[ns--];
 
 	for (m=1; m<=N-1; m++) {
 		for (i=0; i<N-m-1; i++) {
-			number_t factor = (ws.C[i+1] - ws.D[i]) / (p.t[i] - p.t[i+m+1]);
-			ws.C[i] = (p.t[i] - t) * factor ;
-			ws.D[i]= (p.t[i+m+1] - t) * factor;
+			number_t factor = (ws.C[i+1] - ws.D[i]) / (t[i] - t[i+m+1]);
+			ws.C[i] = (t[i] - _t) * factor ;
+			ws.D[i]= (t[i+m+1] - _t) * factor;
 		}
 		dx = (2*ns < (N-m)) ? ws.C[ns+1] : ws.D[ns--];
-		x += dx;
+		_x += dx;
 	}
-	ws.e = dx;
-	return x;
+	*ws.e = dx;
+	return _x;
 };
+
+#include <malloc.h>
 
 int main()
 {
+
+	struct nevilles_algorithm_workspace ws;
+	setup_nevilles_algorithm_workspace(5, &ws, malloc(nevilles_algorithm_workspace_size(5)));
+
 	return 0;
 }
