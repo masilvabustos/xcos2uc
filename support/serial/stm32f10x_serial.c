9b9dbeeb68613d840679abba0f9ed420fac775e0
Matías Silva Bustos <masilvabustos@ieee.org>
2014-10-05 23:52:08 -0300

    modified:   stm32f10x_serial.c

--- support/serial/stm32f10x_serial.c
+++ support/serial/stm32f10x_serial.c
@@ -2,9 +2,19 @@
 #include "serial.h"
 
 
+#include <stm32f10x_usart.h>
+#include <stm32f10x_dma.h>
+
+struct serial_port {
+        USART_TypeDef* base_addr;
+	enum xfer_mode tx_xfer_mode, rx_xfer_mode;
+	uint8_t mask;
+	DMA_TypeDef* tx_dma_channel, rx_dma_channel;
+};
+
 void stm32f10x_set_serial_port_device(struct serial_port *port, USART_TypeDef* device)
 {
-	port->device = device;
+	port->base_addr = device;
 	switch(device){
 	case USART1:
 		port->tx_dma_channel = DMA1_Channel4;
@@ -16,10 +26,52 @@ void stm32f10x_set_serial_port_device(struct serial_port *port, USART_TypeDef* d
 
 };
 
+void setup_serial_port_comm_parameters(struct serial_port *port, struct serial_comm_parameters params)
+{
+	USART_InitTypeDef init;
+	USART_StructInit(&init);
+	
+	init.USART_BaudRate = params.baud_rate;
+	
+	switch (params.word_length) {
+	case 8:
+		if (params.parity == NONE){ 
+			init.USART_WordLength = USART_WordLength_8b;
+			port->mask = 0xff >> params.word_length;
+		} else {
+			init.USART_WordLength = USART_WordLength_9b;
+			init.USART_Parity = (params.parity == EVEN) ? USART_Parity_Even : USART_Parity_Odd;
+		}
+		break;
+	case 9:
+		init.USART_WordLength = USART_WordLength_9b;
+		break;
+	default:
+		break;
+	}
+
+	switch (params.stop_bits) {
+	case _1_STOP_BIT:
+		init.USART_StopBits = USART_StopBits_1;
+		break;
+	case _2_STOP_BITS:
+		init.USART_StopBits = USART_StopBits_2;
+		break;
+	case _1_5_STOP_BITS:
+		init.USART_StopBits = USART_StopBits_1_5;
+		break;
+	default:
+		break;
+	}
+	
+	USART_Init(port->base_addr, &init);
+}
+
 void stm32f10x_reset_serial_port(struct serial_port* port)
 {
-	USART_DeInit(port->device);
+	USART_DeInit(port->base_addr);
 }
+
 void stm32f10x_wait_for_serial_tx(struct serial_port *port)
 {
 	if (port->tx_xfer_mode == XFER_MODE_SYNC)
